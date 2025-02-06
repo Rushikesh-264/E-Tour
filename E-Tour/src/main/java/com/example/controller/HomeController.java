11576d73fbfa8a75bd350d956076fa59fcc7f61c
prithviraj patil <prithvirajpr2003@gmail.com>
2025-02-06 22:09:23 +0530

    ll

--- E-Tour/src/main/java/com/example/controller/HomeController.java
+++ E-Tour/src/main/java/com/example/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.example.services.HomeServices;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class HomeController {
 	@Autowired 
 	private HomeServices homeservices;
